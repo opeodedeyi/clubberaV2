d709e893aec435d28891f8972166b9c1e3d65609
davidtobe13 <davidtobe1999@gmail.com>
2024-08-08 16:41:35 +0100

    changes made on search

--- db.js
+++ db.js
@@ -1,27 +1,3 @@
-// const { Pool } = require('pg')
-// require('dotenv').config()
-
-
-// const DB_PASSWORD = process.env.DB_PASSWORD;
-// const DB_DATABASE = process.env.DB_DATABASE;
-// const DB_USER = process.env.DB_USER;
-// const DB_HOST = process.env.DB_HOST;
-// const DB_PORT = process.env.DB_PORT;
-
-
-// const pool = new Pool({
-//     user: DB_USER,
-//     host: DB_HOST,
-//     database: DB_DATABASE,
-//     password: DB_PASSWORD,
-//     port: DB_PORT,
-//     // ssl: {
-//     //     rejectUnauthorized: false
-//     // }
-// })
-
-// module.exports = pool;
-
 const { Pool } = require('pg');
 require('dotenv').config();
 
@@ -37,18 +13,18 @@ const pool = new Pool({
     database: DB_DATABASE,
     password: DB_PASSWORD,
     port: DB_PORT,
-    // ssl: {
-    //     rejectUnauthorized: false
-    // }
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
-// Test the connection and log a message
+
 pool.connect((err, client, release) => {
     if (err) {
         return console.error('Error acquiring client', err.stack);
     }
     console.log('Database connected successfully');
-    release(); // Release the client back to the pool
+    release(); 
 });
 
 module.exports = pool;
